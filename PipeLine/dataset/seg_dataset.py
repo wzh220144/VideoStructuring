a93722f0685e2f91843efb581f06ebd6d1622c71
Your Name <you@example.com>
2021-06-02 22:38:28 +0800

    update

--- PipeLine/dataset/seg_dataset.py
+++ PipeLine/dataset/seg_dataset.py
@@ -5,12 +5,15 @@ from torch.utils.data import DataLoader, Dataset
 from cachetools import cached, LRUCache, TTLCache
 import numpy as np
 import torch
+import glob
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import tqdm
 
 youtube8m_cache_size = 100000
 stft_cache_size = 100000
 
 class SegDataset(Dataset):
-    def __init__(self, path, extract_youtube8m, extract_stft, postfix, feats_dir):
+    def __init__(self, path, extract_youtube8m, extract_stft, postfix, feats_dir, use_cache):
         self.samples = []
         with open(path, 'r') as fs:
             for line in fs:
@@ -23,6 +26,29 @@ class SegDataset(Dataset):
         self.feats_dir = feats_dir
         self.youtube8m_feats_dir = os.path.join(self.feats_dir, postfix, 'youtube8m')
         self.stft_feats_dir = os.path.join(self.feats_dir, postfix, 'stft')
+        '''
+        if use_cache:
+            self.pre()
+        '''
+        self.use_cache = use_cache
+
+    def pre(self):
+        print('pre start')
+        with ThreadPoolExecutor(max_workers=50) as executor:
+            ps = []
+            for feat_path in os.listdir(self.youtube8m_feats_dir)[:youtube8m_cache_size]:
+                t = feat_path.split('.npy')[0].split('#')
+                video_id = t[0]
+                index = '#' + t[1] + '.npy'
+                ps.append(executor.submit(self.get_youtube8m_feat, video_id, index))
+            for feat_path in os.listdir(self.stft_feats_dir)[:stft_cache_size]:
+                t = feat_path.split('.npy')[0].split('#')
+                video_id = t[0][:len(t[0]) // 2]
+                index = '#' + t[1] + '#' + t[2] + '.npy'
+                ps.append(executor.submit(self.get_stft_feat, video_id, index))
+            for p in tqdm.tqdm(ps, total=len(ps)):
+                p.result()
+        
 
     def __len__(self):
         return len(self.samples)
@@ -33,14 +59,19 @@ class SegDataset(Dataset):
         b1 = sample['b1']
         b2 = sample['b2']
         video_id = sample['id']
-        youtube8m_feat = [torch.from_numpy(self.get_youtube8m_feat(video_id, x)) for x in b1['youtube8m'] + b2['youtube8m']]
-        youtube8m_feat = torch.stack(youtube8m_feat)
-        stft_feat = [torch.from_numpy(self.get_stft_feat(video_id, x)) for x in b1['stft'] + b2['stft']]
-        stft_feat = torch.stack(stft_feat)
+        if self.use_cache:
+            youtube8m_feat = [torch.from_numpy(self.get_youtube8m_feat(video_id, x)) for x in b1['youtube8m'] + b2['youtube8m']]
+            youtube8m_feat = torch.stack(youtube8m_feat).type(torch.FloatTensor)
+            stft_feat = [torch.from_numpy(self.get_stft_feat(video_id, x)) for x in b1['stft'] + b2['stft']]
+            stft_feat = torch.stack(stft_feat).type(torch.FloatTensor)
+        else:
+            youtube8m_feat = [torch.from_numpy(self.get_youtube8m_feat_without_cache(video_id, x)) for x in b1['youtube8m'] + b2['youtube8m']]
+            youtube8m_feat = torch.stack(youtube8m_feat).type(torch.FloatTensor)
+            stft_feat = [torch.from_numpy(self.get_stft_feat_without_cache(video_id, x)) for x in b1['stft'] + b2['stft']]
+            stft_feat = torch.stack(stft_feat).type(torch.FloatTensor)
         return youtube8m_feat, stft_feat, label
 
-    @cached(cache=LRUCache(maxsize=youtube8m_cache_size))
-    def get_youtube8m_feat(self, video_id, index):
+    def get_youtube8m_feat_without_cache(self, video_id, index):
         feat_path = os.path.join(self.youtube8m_feats_dir, '{}{}'.format(video_id, index))
         if os.path.exists(feat_path):
             return np.load(feat_path)
@@ -48,11 +79,22 @@ class SegDataset(Dataset):
             print('{} not exist.'.format(feat_path))
             return np.zeros(1024)
 
-    @cached(cache=LRUCache(maxsize=stft_cache_size))
-    def get_stft_feat(self, video_id, index):
+    def get_stft_feat_without_cache(self, video_id, index):
         feat_path = os.path.join(self.stft_feats_dir, '{}{}{}'.format(video_id, video_id, index))
         if os.path.exists(feat_path):
-            return np.load(feat_path)
+            try:
+                return np.load(feat_path)
+            except:
+                print('load {} failed'.format(feat_path))
+                return np.zeros((257, 90))
         else:
             print('{} not exist.'.format(feat_path))
             return np.zeros((257, 90))
+
+    @cached(cache=LRUCache(maxsize=youtube8m_cache_size))
+    def get_youtube8m_feat(self, video_id, index):
+        return self.get_youtube8m_feat_without_cache(video_id, index)
+
+    @cached(cache=LRUCache(maxsize=stft_cache_size))
+    def get_stft_feat(self, video_id, index):
+        return self.get_stft_feat_without_cache(video_id, index)
--- PipeLine/lgss_train.py
+++ PipeLine/lgss_train.py
@@ -14,8 +14,9 @@ import torch.nn.functional as functional
 import numpy as np
 from sklearn.metrics import average_precision_score
 import tqdm
+import sklearn
 
-os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+os.environ["CUDA_VISIBLE_DEVICES"] = '0,1'
 
 train_iter = 0
 val_iter = 0
@@ -59,10 +60,14 @@ def get_mAP_seq(loader, gts_raw, preds_raw):
         # print(mAP)
     return np.mean(mAP), np.array(mAP)
 
-def train(args, model, data_loader, optimizer, scheduler, epoch, criterion, writer):
+def train(args, model, data_loader, optimizer, scheduler, epoch, criterion, val_loader, writer, max_ap):
     global train_iter
     model.train()
-    #for x in enumerate(data_loader):
+    total_loss = 0.0
+    total_auc = 0.0
+    total_precision = 0.0
+    total_recall = 0.0
+    total_acc = 0.0
     for batch_idx, (youtube8m_data, stft_data, label) in enumerate(tqdm.tqdm(data_loader, total = len(data_loader))):
         label = label.view(-1)
         if args.use_gpu == 1:
@@ -74,18 +79,83 @@ def train(args, model, data_loader, optimizer, scheduler, epoch, criterion, writ
         output = output.view(-1, 2)
         loss = criterion(output, label)
         loss.backward()
+        prob = functional.softmax(output, dim=1)[:, 1].cpu().detach().numpy()
+        total_loss += loss.item()
+        pred = np.nan_to_num(prob) > 0.5
         optimizer.step()
-        if loss.item() > 0:
-            train_iter += 1
-            writer.add_scalar('train/loss', loss.item(), train_iter)
-        print('epoch {}: [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-            epoch,
-            int(batch_idx * len(youtube8m_data)),
-            len(data_loader.dataset), 100. * batch_idx / len(data_loader),
-            loss.item()))
-    scheduler.step()
+        train_iter += 1
 
-def test(args, model, data_loader, criterion, writer):
+        auc = 0
+        try:
+            auc = sklearn.metrics.roc_auc_score(labels, probs)
+        except:
+            auc = 1.0
+        _label = label.cpu().detach().numpy().tolist()
+        _pred = pred.tolist()
+        acc = sklearn.metrics.accuracy_score(_label, _pred)
+        recall = sklearn.metrics.recall_score(_label, _pred, zero_division=1)
+        precision = sklearn.metrics.precision_score(_label, _pred, zero_division=1)
+        total_acc += acc
+        total_recall += recall
+        total_precision += precision
+        total_auc += auc
+
+        writer.add_scalar('train/loss', total_loss / (batch_idx + 1), train_iter)
+        writer.add_scalar('train/auc', total_auc / (batch_idx + 1), train_iter)
+        writer.add_scalar('train/acc', total_auc / (batch_idx + 1), train_iter)
+        writer.add_scalar('train/recall', total_recall / (batch_idx + 1), train_iter)
+        writer.add_scalar('train/precision', total_precision / (batch_idx + 1), train_iter)
+        if batch_idx % 100 == 0:
+            t = 'epoch {}: [{}/{} ({:.0f}%)]\t' + \
+                'cur loss: {:.6f}, avg loss: {:.6f}, cur auc: {:.6f}, avg auc: {:.6f},' + \
+                ' cur acc: {:.6f}, avg acc: {:.6f}, cur recall: {:.6f}, avg recall: {:.6f},' + \
+                ' cur precision: {:.6f}, avg precision: {:.6f}'
+            print(t.format(
+                epoch,
+                int(batch_idx * len(youtube8m_data)),
+                len(data_loader.dataset),
+                100. * batch_idx / len(data_loader),
+                loss.item(),
+                total_loss / (batch_idx + 1),
+                auc,
+                total_auc / (batch_idx + 1),
+                acc,
+                total_auc / (batch_idx + 1),
+                recall,
+                total_recall / (batch_idx + 1),
+                precision,
+                total_precision / (batch_idx + 1)
+                ))
+        if batch_idx % 300 == 0:
+            print('start val...')
+            ap = test(args, model, val_loader, criterion, writer, 100)
+            if ap > max_ap:
+                is_best = True
+                max_ap = ap
+            else:
+                is_best = False
+            print('epoch {}: [{}/{} ({:.0f}%)]\tcur loss: {:.6f}, avg loss: {:.6f}, auc: {:.6f}, acc: {:.6f}, recall: {:.6f}, precision: {:.6f}, ap: {:.6f}, max_ap: {:6f}'.format(
+                epoch,
+                int(batch_idx * len(youtube8m_data)),
+                len(data_loader.dataset),
+                100. * batch_idx / len(data_loader),
+                loss.item(),
+                total_loss / (batch_idx + 1),
+                auc,
+                acc,
+                recall,
+                precision,
+                ap,
+                max_ap
+                ))
+            
+            save_checkpoint({'state_dict': model.state_dict(), 'epoch': epoch + 1,}, 
+                    is_best = is_best, fpath = osp.join(args.model_dir, 'checkpoint.pth.tar'))
+
+    scheduler.step()
+    return max_ap
+
+def test(args, model, data_loader, criterion, writer, max_batch = -1):
     global val_iter
     model.eval()
     val_loss = 0
@@ -95,7 +165,7 @@ def test(args, model, data_loader, criterion, writer):
     preds, gts = [], []
     batch_num = 1  # 0
     with torch.no_grad():
-        for youtube8m_data, stft_data, label in data_loader:
+        for youtube8m_data, stft_data, label in tqdm.tqdm(data_loader, total = len(data_loader)):
             batch_num += 1
             label = label.view(-1)
             if args.use_gpu == 1:
@@ -126,6 +196,8 @@ def test(args, model, data_loader, criterion, writer):
             all_gt += len(gt)
             correct1 += len(np.where(gt[idx1] == prediction[idx1])[0])
             correct0 += len(np.where(gt[idx0] == prediction[idx0])[0])
+            if max_batch != -1 and batch_num >= max_batch:
+                break
         for x in gts_raw:
             gts.extend(x.tolist())
         for x in prob_raw:
@@ -150,8 +222,8 @@ def main():
     parser.add_argument('--samples_dir', type = str, default = '/home/tione/notebook/VideoStructuring/dataset/samples/seg')
     parser.add_argument('--model_dir', type = str, default = '/home/tione/notebook/VideoStructuring/dataset/model/seg')
     parser.add_argument('--feats_dir', type = str, default = '/home/tione/notebook/VideoStructuring/dataset/feats')
-    parser.add_argument('--youtube8m_cache_size', type = int, default = 1000000)
-    parser.add_argument('--stft_cache_size', type = int, default = 1000000)
+    parser.add_argument('--youtube8m_cache_size', type = int, default = 10000)
+    parser.add_argument('--stft_cache_size', type = int, default = 10000)
     parser.add_argument('--batch_size', type = int, default = 32)
     parser.add_argument('--extract_youtube8m', type = bool, default = True)
     parser.add_argument('--extract_stft', type = bool, default = True)
@@ -174,10 +246,19 @@ def main():
     seg_dataset.stft_cache_size = args.stft_cache_size
 
     train_samples_path = os.path.join(args.samples_dir, 'train')
-    val_samples_path = os.path.join(args.samples_dir, 'test')
+    val_samples_path = os.path.join(args.samples_dir, 'val')
 
-    train_loader = DataLoader(SegDataset(train_samples_path, args.extract_youtube8m, args.extract_stft, 'train_5k_A', args.feats_dir), batch_size=args.batch_size, shuffle=True)
-    val_loader = DataLoader(SegDataset(val_samples_path, args.extract_youtube8m, args.extract_stft, 'train_5k_A', args.feats_dir), batch_size=args.batch_size, shuffle=False)
+    train_loader = DataLoader(
+            SegDataset(train_samples_path, args.extract_youtube8m, args.extract_stft, 'train_5k_A', args.feats_dir, False), 
+            num_workers = 10,
+            prefetch_factor = 100,
+            batch_size=args.batch_size, 
+            shuffle=True)
+    val_loader = DataLoader(
+            SegDataset(val_samples_path, args.extract_youtube8m, args.extract_stft, 'train_5k_A', args.feats_dir, False), 
+            num_workers = 10,
+            prefetch_factor = 100,
+            batch_size=args.batch_size, shuffle=False)
 
     model = LGSS(args)
     if args.use_gpu == 1:
@@ -188,7 +269,7 @@ def main():
         model.load_state_dict(checkpoint['state_dict'])
 
     optimizer = Adam(model.parameters(), lr = 1e-3, weight_decay=5e-4)
-    scheduler = MultiStepLR(optimizer, milestones = [10, 30])
+    scheduler = MultiStepLR(optimizer, milestones = [1000, 3000])
     criterion = nn.CrossEntropyLoss(torch.Tensor([0.1, 1]))
     if args.use_gpu == 1:
         criterion = criterion.cuda()
@@ -200,7 +281,7 @@ def main():
     writer = SummaryWriter(logdir=args.logs_dir)
     max_ap = -1
     for epoch in range(args.epochs):
-        train(args, model, train_loader, optimizer, scheduler, epoch, criterion, writer)
+        max_ap = train(args, model, train_loader, optimizer, scheduler, epoch, criterion, val_loader, writer, max_ap)
         print("Val Acc")
         ap = test(args, model, val_loader, criterion, writer)
         if ap > max_ap:
