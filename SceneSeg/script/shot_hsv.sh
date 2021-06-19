94fa4f27530fc75912f9b8dae11f6a4eb0942eb3
Your Name <you@example.com>
2021-06-19 21:01:25 +0800

    update

--- SceneSeg/script/shot_hsv.sh
+++ SceneSeg/script/shot_hsv.sh
@@ -6,4 +6,5 @@
 # Brief: 
 # Version: 1.0
 #########################################################################
-python -u /home/tione/notebook/VideoStructuring/SceneSeg/pre/ShotDetect/shotdetect_hsv.py
+#python -u /home/tione/notebook/VideoStructuring/SceneSeg/pre/ShotDetect/shotdetect_hsv.py
+python -u /home/tione/notebook/VideoStructuring/SceneSeg/pre/ShotDetect/shotdetect_hsv.py --video_dir=/home/tione/notebook/dataset/videos/test_5k_2nd --save_dir=/home/tione/notebook/dataset/test_5k_2nd/shot_hsv
